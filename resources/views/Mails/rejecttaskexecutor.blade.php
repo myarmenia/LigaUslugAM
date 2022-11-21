f6a55ee3fb2ad41154dd47b4c58b7dfea6c5e79a
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2022-11-21 16:38:23 +0400

    AKJJJ

--- resources/views/Mails/rejecttaskexecutor.blade.php
+++ resources/views/Mails/rejecttaskexecutor.blade.php
@@ -12,8 +12,8 @@
             <div class="row">
                 <div class="col-sm-3 col-xs-12  mb-3">
                    <h3> 'Уважаемый(ая) Исполнитель, </h3>
-        {{dd($click_on_task)}}
-                   <p class="m-2">спасибо за  ваш отклик на заказ " {{ $click_on_task->tasks->title }}", в этот раз клиент выбрал нужного специалиста.</p>
+
+                   <p class="m-2">спасибо за  ваш отклик на заказ " {{ $click_on_task }}", в этот раз клиент выбрал нужного специалиста.</p>
 
                     <h5>С уважением</h5>
                     <div  style="height:70px;width:300px;display:flex;align-items:center">
