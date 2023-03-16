77625cd07d5169b1adaf30c23df9c9c9486d77ec
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2023-03-16 15:37:47 +0400

    AK16.03.23:15:37

--- resources/views/admin/answer_and_question/index.blade.php
+++ resources/views/admin/answer_and_question/index.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.admin_app')
 @section('content')
 <section id="main-content" >
-    <section class="wrappe " style="height:700px;margin-top:70px">
+    <section class="wrappe py-5" style="min-height:800px;margin-top:70px">
         <div class="container">
             <div class="row"></div>
             <div class="bg-white p-3 mt-2" >
@@ -13,7 +13,7 @@
                         <th scope="col" style="width:100px">№</th>
                         <th scope="col" style="width:250px">Вопрос</th>
                         <th scope="col" style="width:250px">Ответ</th>
-                        <th scope="col" class= "text-center" colspan=2>Редактировать</th>
+                        <th scope="col"  class= "text-center" colspan=2>Редактировать</th>
 
                     </tr>
                     </thead>
@@ -21,9 +21,9 @@
                         @foreach( $get_answer_and_question as $items)
                             <tr>
                                 <td>{{ $items->id }}</td>
-                                <td style="width:100px">{{$items->question}}</td>
-                                <td style="width:100px">{{$items->answer}}</td>
-                                <td style="width:100px" class="text-center">
+                                <td><div style="height:200px;overflow-y:scroll">{{$items->question}}</div></td>
+                                <td><div style="height:200px;overflow-y:scroll">{{$items->answer}}</div></td>
+                                <td style="width:100px;" class="text-center">
                                      <a href="{{ route ('editanswer',$items->id) }}" class="btn btn-info btn-sm mr-1 "><i class="fa fa-edit"></i></a>
                                      <button id="deletequestion"  data-id="{{$items->id}}"  class="btn my-2" style="background:#394a59;color:#fff"><i class="fa fa-trash"></i></button>
                                 </td>
