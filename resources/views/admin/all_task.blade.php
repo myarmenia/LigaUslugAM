f18e47fb8c293c327bcdb6ca83566a0e3a2ce92b
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2023-05-01 10:07:07 +0400

    AK01.05.23:10:07

--- resources/views/admin/all_task.blade.php
+++ resources/views/admin/all_task.blade.php
@@ -94,7 +94,8 @@
                             @foreach( $task as $items)
                                     <tr>
                                         <td>{{ $items->id }}</td>
-                                        <td>{{ $items->title }}</td>
+                                      
+                                        <td>{{ $items->id }}</td>
 
                                         <td class="text-center">
                                             <a  href = "{{ route('task.show', $items->id )}}">
