3f8dec052ea9ead0549291d6fccabe75795ac7d5
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2023-09-19 16:47:17 +0400

    region name change

--- app/Http/Controllers/Pages/FindTaskController.php
+++ app/Http/Controllers/Pages/FindTaskController.php
@@ -57,7 +57,7 @@ class FindTaskController extends Controller
             $query->whereIn('subcategory_name', $json_decode_subcategory)->with('users');
 
             if($request->region_name!=null){
-                dd($request->region_name);
+         
                 $query->where('region',$request->region_name);
             }
 
