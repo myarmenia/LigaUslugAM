f75deb1bbfc33660b4550f0b436d54c3f0773677
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-02-22 12:35:12 +0400

    open_kernel_comments_22.02.24

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,14 +30,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('deadline:cron')->everyMinute();//done
-        // $schedule->command('newjob:cron')->dailyAt('08:00'); //done
+        $schedule->command('deadline:cron')->everyMinute();//done
+        $schedule->command('newjob:cron')->dailyAt('08:00'); //done
 
-        // $schedule->command('deletenotappliedtask:cron')->everyMinute();//done
-        // $schedule->command('returnedmoney:cron')->dailyAt('08:00');
-        // $schedule->command('notclickfromtwodays:cron')->dailyAt('08:00');
-        $schedule->command('notclickfromtwodays:cron')->everyMinute();//test
-        // $schedule->command('notclickfromthreedays:cron')->dailyAt('08:00');
+        $schedule->command('deletenotappliedtask:cron')->everyMinute();//done
+        $schedule->command('returnedmoney:cron')->dailyAt('08:00');
+        $schedule->command('notclickfromtwodays:cron')->dailyAt('08:00');
+
+        $schedule->command('notclickfromthreedays:cron')->dailyAt('08:00');
 
 
     }
